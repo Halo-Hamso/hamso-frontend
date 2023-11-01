b3a598cae5046a4f2ac7a196ed8e211156934504
최원서 <cwonseo@choewonseoui-MacBookAir.local>
2023-11-01 19:50:01 +0900

    chore: 콘솔 로그 수정

--- src/Apis/CustomApis.js
+++ src/Apis/CustomApis.js
@@ -2,6 +2,5 @@ import axios from 'axios';
 const API = process.env.REACT_APP_API;
 
 export const visitInfoApi = (visitInfo) => {
-  console.log(API);
   return axios.post(`${API}/account-book/account-info`, { visitInfo });
 };
